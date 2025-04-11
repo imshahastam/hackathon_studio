8afcab43ec38ec0efed7b22e5ad567deef66882e
imshahastam <shahzada.stamova@gmail.com>
2025-04-11 11:17:13 +0600

    rewrite login api

--- src/main/java/com/shaha/hackathon/controller/LoginController.java
+++ src/main/java/com/shaha/hackathon/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.shaha.hackathon.controller;
 
+import com.shaha.hackathon.exceptions.MessageResponse;
 import com.shaha.hackathon.security.jwt.JwtUtil;
+import com.shaha.hackathon.security.jwt.TokenDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,10 +10,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class LoginController {
     private final AuthenticationManager manager;
@@ -23,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody com.shaha.hackathon.user.User user) {
+    public ResponseEntity<TokenDTO> login(@RequestBody com.shaha.hackathon.user.User user) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
                 user.getUsername(),
                 user.getPassword()
@@ -33,6 +37,6 @@ public class LoginController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String jwtToken = JwtUtil.generateToken((User) authentication.getPrincipal());
-        return ResponseEntity.ok(jwtToken);
+        return ResponseEntity.ok().body(new TokenDTO(jwtToken, user.getUsername()));
     }
 }
